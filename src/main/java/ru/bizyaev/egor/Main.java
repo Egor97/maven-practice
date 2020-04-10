06d9368d5451ee8a1ad37d0d695263f1326ec8a6
Egor Bizyaev <egorbizyaev123@gmail.com>
2020-04-10 15:02:05 +0400

    feat: add javadoc plugin in pom.xml file

--- src/main/java/ru/bizyaev/egor/Main.java
+++ src/main/java/ru/bizyaev/egor/Main.java
@@ -2,10 +2,14 @@ package ru.bizyaev.egor;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class Main {
+
+    /**
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
         System.out.println(NumberUtils.max(12, 43, 1, 5));
     }
 }
